cf63b50ea320e25f0f1371c7687b49d8d20df54f
Tointer <Tointer@yandex.ru>
2025-05-31 15:44:36 +0300

    create basic ent schema

--- ent/schema/media.go
+++ ent/schema/media.go
@@ -1,6 +1,10 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
 
 // Media holds the schema definition for the Media entity.
 type Media struct {
@@ -9,10 +13,26 @@ type Media struct {
 
 // Fields of the Media.
 func (Media) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Int("id").
+			Unique().
+			Immutable().
+			Comment("Unique identifier for the media item"),
+		field.String("hash").
+			Unique().
+			Immutable().
+			Comment("Hash of the media file, used for deduplication"),
+		field.Enum("type").
+			Values("image", "video", "audio").
+			Immutable().
+			Comment("Type of the media, can be image, video, or audio"),
+	}
 }
 
 // Edges of the Media.
 func (Media) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("tags", Tag.Type).
+			Comment("Tags associated with the media item, used for categorization"),
+	}
 }
--- ent/schema/tag.go
+++ ent/schema/tag.go
@@ -1,6 +1,9 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Tag holds the schema definition for the Tag entity.
 type Tag struct {
@@ -9,7 +12,20 @@ type Tag struct {
 
 // Fields of the Tag.
 func (Tag) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Int("id").
+			Unique().
+			Immutable().
+			Comment("Unique identifier for the tag"),
+		field.String("name").
+			Unique().
+			Immutable().
+			Comment("Name of the tag, used for categorization"),
+		field.Enum("type").
+			Values("user_tag", "meta_tag").
+			Immutable().
+			Comment("Type of the tag, can be user_tag or meta_tag"),
+	}
 }
 
 // Edges of the Tag.
