14f83cdf80148300620bff454695b3e1b7c8fe97
Tointer <Tointer@yandex.ru>
2025-06-11 01:44:00 +0300

    small styling change

--- web/src/lib/components/media/MediaCard.svelte
+++ web/src/lib/components/media/MediaCard.svelte
@@ -12,6 +12,6 @@
         class="h-auto w-full rounded-md object-contain shadow"
         loading="lazy"
     />
-    <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-10 transition-opacity rounded-md duration-200"></div>
+    <div class="absolute inset-0 bg-black opacity-0 opacity-3 group-hover:opacity-12 transition-opacity rounded-md duration-200"></div>
 </a>
 
