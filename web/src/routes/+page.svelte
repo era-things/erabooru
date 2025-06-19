cbe2575d1e57847793609c6ff08362f03adc6d9e
Tointer <Tointer@yandex.ru>
2025-06-19 20:31:06 +0300

    fix svelte errors

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
-	import MediaGrid from '$lib/components/MediaGrid.svelte';
-	import TabNav from '$lib/components/TabNav.svelte';
-	import { page } from '$app/stores';
+        export let data : {query: string};
+        import MediaGrid from '$lib/components/MediaGrid.svelte';
+        import TabNav from '$lib/components/TabNav.svelte';
+	import type { LoadEvent } from '@sveltejs/kit';
 
-	$: query = $page.url.searchParams.get('q') ?? '';
+        export const load = ({ url }: LoadEvent) => {
+                return {
+                        query: url.searchParams.get('q') ?? ''
+                };
+        };
+
+        let query = data.query;
+        // $: query = $page.url.searchParams.get('q') ?? '';
 </script>
 
-<div class="h-screen">
-	<TabNav active="media" />
-	<MediaGrid {query} />
+<div class="h-screen ">
+        <TabNav active="media" />
+        <MediaGrid {query} />
 </div>
