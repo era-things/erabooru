8282f203e73f87ab6d384aa8f3c7a2aba459e7a4
Tointer <Tointer@yandex.ru>
2025-09-28 05:48:08 +0300

    fix ci error

--- internal/embeddings/dummy.go
+++ internal/embeddings/dummy.go
@@ -1,3 +1,11 @@
 //go:build !embeddings
 
 package embed
+
+// Dummy stubs for vet/test without embeddings tag.
+func VisionEmbedding(any interface{}) ([]float32, error) {
+	return nil, nil
+}
+func TextEmbedding(any interface{}) ([]float32, error) {
+	return nil, nil
+}
