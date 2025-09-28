0eaa7f49522496558293f70b821d39fe1ce0f18f
Tointer <Tointer@yandex.ru>
2025-09-28 05:59:56 +0300

    fix dummy

--- internal/embeddings/dummy.go
+++ internal/embeddings/dummy.go
@@ -2,15 +2,31 @@
 
 package embed
 
+import (
+	"context"
+)
+
 // Dummy stubs for vet/test without embeddings tag.
+
 func VisionEmbedding(any interface{}) ([]float32, error) {
 	return nil, nil
 }
+
 func TextEmbedding(any interface{}) ([]float32, error) {
 	return nil, nil
 }
+
 func DefaultModelOptionsFromEnv() ModelOptions {
 	return ModelOptions{}
 }
 
+func EnsureModel(ctx context.Context, opts ModelOptions) (string, error) {
+	return "", nil
+}
+
+func Load(dir string) error {
+	return nil
+}
+
+// Add any types referenced in main.go or workers
 type ModelOptions struct{}
